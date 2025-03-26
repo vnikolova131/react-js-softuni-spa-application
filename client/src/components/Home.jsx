6e6f150a2d79231dd42d42d3bbdaa40bbb341938
vnikolova131 <130762543+vnikolova131@users.noreply.github.com>
2025-03-26 12:49:13 +0200

    Added server to the app

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
         <div
           aria-hidden="true"
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        >
+        > 
           <div
             style={{
               clipPath:
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  
     <App />
+
   </BrowserRouter>,
 )
