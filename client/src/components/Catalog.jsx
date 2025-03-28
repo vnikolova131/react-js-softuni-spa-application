d2ac5efdab1e7a5be015a01e28ada6c693419b2c
vnikolova131 <130762543+vnikolova131@users.noreply.github.com>
2025-03-29 00:12:28 +0200

    fixed image urls so they show the pictures chosen

--- client/src/components/Catalog.jsx
+++ client/src/components/Catalog.jsx
@@ -5,7 +5,7 @@ import groupService from "../services/groupService";
 
   export default function Catalog() {
 
-  const [groups, setGroups] =  useState([])
+  const [group, setGroups] =  useState([])
    useEffect(() => {
     groupService.getAll()
       .then(setGroups)
@@ -17,17 +17,17 @@ import groupService from "../services/groupService";
           <h2 className="sr-only">Groups</h2>
   
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {groups.map((groups) => (
-              <Link key={groups._id} to={`/details`} className="group">
+            {group.map((group) => (
+              <Link key={group._id} to={`/details`} className="group">
                 <img
-                  alt={groups.imageAlt}
-                  src={groups.imageUrl}
+                  src={group.urlImage}
                   className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
                 />
-                <h3 className="mt-4 text-sm text-gray-700">{groups.level}</h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">{groups.name}</p>
+                <h3 className="mt-4 text-sm text-gray-700">{group.level}</h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">{group.name}</p>
               </Link>
             ))}
+            {group.length === 0 && <h3 className="no-articles">No groups created yet!</h3>}
           </div>
         </div>
       </div>
