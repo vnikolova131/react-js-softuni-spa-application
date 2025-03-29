f06bc2ba27939bacc9f5892138b1d053429624cb
vnikolova131 <130762543+vnikolova131@users.noreply.github.com>
2025-03-29 10:39:41 +0200

    added function to take email and password from login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { useState } from 'react'
-import { ChevronDownIcon } from '@heroicons/react/16/solid'
 import { Field, Label, Switch } from '@headlessui/react'
 import { Link } from 'react-router'
 
 export default function Login() {
-  const [agreed, setAgreed] = useState(false)
+
+  const loginAction = (formData) => {
+    const email = formData.get('email')
+  }
 
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
@@ -26,7 +28,7 @@ export default function Login() {
         <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Log in</h2>
         <p className="mt-2 text-lg/8 text-gray-600">Access your acount here!</p>
       </div>
-      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+      <form action={loginAction} className="mx-auto mt-16 max-w-xl sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div className="sm:col-span-2">
             <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
