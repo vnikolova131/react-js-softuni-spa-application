c655b83e65ec8095a7b8c026ad49cd61906371fd
vnikolova131 <130762543+vnikolova131@users.noreply.github.com>
2025-03-30 11:09:28 +0300

    add abort con troller to custom login hook

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,13 +1,23 @@
+import { useEffect, useRef } from "react";
 import request from "../utils/request"
 
 const baseUrl = `http://localhost:3030/users`
 
 export const useLogin = () => {
+    const abortRef = useRef();
+
     const login = async (email, password) => {
-      const result = await request.post(`${baseUrl}/login`, { email,password });
+      const result = await request.post(`${baseUrl}/login`, { email,password }, { signal: abortRef.current.signal });
 
       return result;
     }
+
+    useEffect(() => {
+        const abortController = new AbortController();
+        abortRef.current = abortController
+
+        return () => abortController.abort()
+    }),[];
 
     return {
         login,
